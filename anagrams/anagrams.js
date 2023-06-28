84b31676fb3e33c425f3eb017dde127c562c6118
Anna Gibes <gibesav@gmail.com>
2023-06-28 18:29:54 -0500

    updates anagrams input/output

--- anagrams/anagrams.js
+++ anagrams/anagrams.js
@@ -3,31 +3,40 @@
 const anagrams = () => {
     const root = document.querySelector('#root');
     root.appendChild(Object.assign(document.createElement('h1'), {id: 'title'})).appendChild(document.createTextNode('Anagrams'));
-    root.appendChild(Object.assign(document.createElement('input'), {id: 'input', type: 'text'}));
+    root.appendChild(Object.assign(document.createElement('input'), {id: 'input1', type: 'text'}));
+    root.appendChild(Object.assign(document.createElement('input'), {id: 'input2', type: 'text'}));
     root.appendChild(Object.assign(document.createElement('p'), {id: 'p'}));
 
-    const input = document.querySelector('#input');
+    const input1 = document.querySelector('#input1');
+    const input2 = document.querySelector('#input2');
     const p = document.querySelector('#p');
 
-    input.addEventListener("input", () => {
-        p.textContent = 'checking for anagrams....';
-        const charArr = [...input.value]
-        
-        var charObj = {};
+    var charCount1;
+    var charCount2;
 
-        charArr.filter(char => char != ' ').map(char => {
-            Object.keys(charObj).includes(char) ? charObj[char] += 1: charObj[char] = 1;
-        }) 
+    const isAnagram = () => {
+        if(charCount1 === charCount2) {
+            p.textContent = 'it is an anagram';
+        } else {
+            p.textContent = 'not an anagram';
+        }
+    }
 
-        const countArr = Object.values(charObj);
+    input1.addEventListener("input", () => {
+        const charArr1 = [...input1.value];    
+        charCount1 = charArr1.length;
 
-        const highestCount = countArr.find(count => count === Math.max(...countArr));
-        const lowestCount = countArr.find(count => count === Math.min(...countArr));
-
-        (highestCount === lowestCount) ? p.textContent = 'it is an anagram' : p.textContent = 'not an anagram';
-
+        isAnagram();
     });
 
+    input2.addEventListener("input", () => {
+        const charArr2 = [...input2.value];
+        charCount2 = charArr2.length;
+
+        isAnagram();
+    });
 }
 
 anagrams();
+    
+//does not check for capital vs lowercase vs number
