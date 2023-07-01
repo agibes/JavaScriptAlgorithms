11bc41217c980b9ae72af2c0e31891fa5bd64887
Anna Gibes <gibesav@gmail.com>
2023-06-30 20:25:35 -0500

    adds reverse-words

---
+++ reverse-words/reverse-words.js
@@ -0,0 +1,24 @@
+//Given a phrase, reverse the order of the characters of each word.
+
+const reverseWords = () => {
+    const root = document.querySelector('#root');
+    root.appendChild(Object.assign(document.createElement('h1'), {id: 'title'})).appendChild(document.createTextNode('Word Reversal'));
+    root.appendChild(Object.assign(document.createElement('input'), {id: 'input', type: 'text'}));
+    root.appendChild(Object.assign(document.createElement('p'), {id: 'p'}))
+
+    const input = document.querySelector('#input');
+    const p = document.querySelector('p');
+
+    input.addEventListener("input", () => {
+        var arr = [...input.value.split(" ")];
+        var newArr = []
+        arr.forEach(item => {
+            var stringArr = [...item]
+            stringArr.reverse();
+            newArr.push(stringArr.join(''));
+        })
+        p.textContent = newArr.join(' ');
+    })
+}
+
+reverseWords();
