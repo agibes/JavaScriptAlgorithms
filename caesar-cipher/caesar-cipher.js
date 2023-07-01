08cec6b8e51953797212301cc5cedf8ee9c2b7e2
Anna Gibes <gibesav@gmail.com>
2023-07-01 17:29:43 -0500

    adds caesar-cipher

---
+++ caesar-cipher/caesar-cipher.js
@@ -0,0 +1,36 @@
+// Given a phrase, substitute each character by shifting it up or down the alphabet by a given integer. 
+// If necessary, the shifting should wrap around back to the beginning or end of the alphabet.
+
+const caesarCipher = () => {
+    const root = document.querySelector('#root');
+    root.appendChild(Object.assign(document.createElement('h1'), {id: 'title'})).appendChild(document.createTextNode('Caesar Cipher'));
+    root.appendChild(Object.assign(document.createElement('input'), {id: 'input1', type: 'text'}));
+    root.appendChild(Object.assign(document.createElement('input'), {id: 'input2', type: 'number'}));
+
+    root.appendChild(Object.assign(document.createElement('p'), {id: 'p'}))
+
+    const input1 = document.querySelector('#input1');
+    const input2 = document.querySelector('#input2');
+    const input = document.querySelectorAll('input');
+    const p = document.querySelector('p');
+
+    const alphabetArr = 'abcdefghijklmkopqrstuvwxyz'.split('');
+
+    input.forEach(input => {
+        input.addEventListener("input", () => {
+            var stringArr = [...input1.value.toLowerCase()];
+            var num = parseInt(input2.value);
+            for (let i =0; i<stringArr.length; i++) {
+                const findLetter = (e) => e == stringArr[i];
+                var sum = num + alphabetArr.findIndex(findLetter);
+                if (stringArr[i] === ' ') {continue};
+                while (sum >= alphabetArr.length) {sum -= 26};
+                stringArr[i] = alphabetArr[sum];
+            };
+            p.textContent = stringArr.join('');
+        });
+    })
+}
+
+
+caesarCipher();
