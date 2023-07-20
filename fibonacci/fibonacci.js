bb6dabbf8154d7cfa86caced63c15233e2e1774d
Anna Gibes <gibesav@gmail.com>
2023-07-20 10:40:37 -0500

    adds fibonacci number

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,30 @@
+//Implement a function that returns the fibonacci number at a given index.
+
+const fibNum = () => {
+    //create the page content
+    const root = document.querySelector('#root');
+    root.appendChild(Object.assign(document.createElement('h1'), {id: 'title'})).appendChild(document.createTextNode('Fibonacci Number'));
+    root.appendChild(Object.assign(document.createElement('input'), {id: 'input', type: 'number'}))
+    root.appendChild(Object.assign(document.createElement('p'), {id: 'p'}))
+
+    const input = document.querySelector('#input');
+    const p = document.querySelector('p');
+
+    input.addEventListener("input", () => {
+        //find the fibonacci number at index (input)
+        let target = input.value;
+        let fibonacci = [0, 1, 1]
+
+        for (let current = 3; current <= target; current++) {
+            let prev = fibonacci[current - 1];
+            let _prev = fibonacci[current - 2];
+            fibonacci.push(prev + _prev);
+        }
+
+        p.textContent = 'The fibonacci number at index ' + target + ' is ' + fibonacci[target];
+
+        return fibonacci[target];
+    })
+}
+
+fibNum();
